3b1da7094e4c8eef5d120e0e47eed07f904dcdd3
nyambayar0613 <nyamka0613@gmail.com>
2021-05-08 20:35:39 +0800

    home page zasav

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/n0m4dz/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/n0m4dz/Workspace/mobile/youth"
+export "FLUTTER_ROOT=/Users/nyambayar/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/nyambayar/workspace/youth-app"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
