c8d6a10e3ef4587a0373e3d7371f918cdc81b729
nyambayar0613 <nyamka0613@gmail.com>
2021-07-18 15:40:50 +0800

    fix

--- lib/ui/views/pages/lawPage/lawDetail.dart
+++ lib/ui/views/pages/lawPage/lawDetail.dart
@@ -5,6 +5,7 @@ import 'package:flutter_html/style.dart';
 import 'package:youth/core/constants/values.dart';
 import 'package:youth/core/models/law.dart';
 import 'package:youth/core/models/volunteer_work.dart';
+import 'package:youth/ui/components/header-back.dart';
 import 'package:youth/ui/styles/_colors.dart';
 import 'package:flutter/rendering.dart';
 
@@ -23,144 +24,115 @@ class LawDetailPageState extends State<LawDetailPage> {
     var size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: bgColor,
-      body: Container(
-        width: double.infinity,
-        decoration: BoxDecoration(
-          color: lawColor,
-          image: DecorationImage(
-            image: AssetImage("assets/images/page-heading-law.jpg"),
-            alignment: Alignment.topRight,
-            //fit: BoxFit.fitWidth,
-          ),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(50.0),
+        child: HeaderBack(
+          title: 'Хууль',
+          reversed: true,
         ),
-        child: Column(
-          children: [
-            Container(
-              height: widget.item.title.length > 60 ? 210 : 170,
-              padding: EdgeInsets.only(
-                top: 60,
-                right: 20,
-              ),
-              child: Column(
-                children: [
-                  SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      IconButton(
-                        onPressed: () => Navigator.of(context).pop(false),
-                        icon: Icon(
-                          Icons.arrow_back,
-                          color: Colors.white,
-                        ),
-                      ),
-                      Expanded(
-                        child: Text(
-                          widget.item.title != null
-                              ? widget.item.title.toUpperCase()
-                              : 'Хууль'.toUpperCase(),
-                          style: TextStyle(
-                            fontSize: 14,
-                            color: Colors.white,
-                            fontWeight: FontWeight.w600,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                ],
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: lawColor,
+            ),
+            child: Text(
+              widget.item.title,
+              style: TextStyle(
+                color: Colors.white,
               ),
             ),
-            Expanded(
-              child: SingleChildScrollView(
-                child: Container(
-                  width: double.infinity,
-                  height: size.height,
-                  padding: EdgeInsets.symmetric(horizontal: 15),
-                  decoration: BoxDecoration(
-                    //borderRadius: BorderRadius.circular(10),
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(15),
-                      topRight: Radius.circular(15),
-                    ),
-                    color: bgColor,
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Container(
+                width: double.infinity,
+                height: size.height,
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                decoration: BoxDecoration(
+                  //borderRadius: BorderRadius.circular(10),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15),
                   ),
-                  child: Column(
-                    children: [
-                      SizedBox(height: 10),
-                      widget.item.thumb != null
-                          ? Image.network(baseUrl + widget.item.thumb)
-                          : Image.network(
-                              baseUrl + "/assets/youth/images/noImage.jpg"),
-                      SizedBox(height: 10),
-                      Container(
-                        //child: Html(data: widget.news.content),
-                        child: widget.item.body != null
-                            ? Html(
-                                data: widget.item.body.toString(),
-                                style: {
-                                  "p": Style(
-                                    color: kTextColor,
-                                    fontSize: FontSize(13),
-                                    fontWeight: FontWeight.normal,
-                                  ),
-                                },
-                              )
-                            : Text(''),
+                  color: bgColor,
+                ),
+                child: Column(
+                  children: [
+                    SizedBox(height: 10),
+                    widget.item.thumb != null
+                        ? Image.network(baseUrl + widget.item.thumb)
+                        : Image.network(
+                            baseUrl + "/assets/youth/images/noImage.jpg"),
+                    SizedBox(height: 10),
+                    Container(
+                      //child: Html(data: widget.news.content),
+                      child: widget.item.body != null
+                          ? Html(
+                              data: widget.item.body.toString(),
+                              style: {
+                                "p": Style(
+                                  color: kTextColor,
+                                  fontSize: FontSize(13),
+                                  fontWeight: FontWeight.normal,
+                                ),
+                              },
+                            )
+                          : Text(''),
+                    ),
+                    Container(
+                      height: 90,
+                      margin: EdgeInsets.only(
+                        left: 10,
+                        right: 10,
+                        top: 30,
+                        bottom: 10,
                       ),
-                      Container(
-                        height: 90,
-                        margin: EdgeInsets.only(
-                          left: 10,
-                          right: 10,
-                          top: 30,
-                          bottom: 10,
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border(
-                            top: BorderSide(
-                              color: Colors.grey,
-                              width: 1,
-                            ),
+                      decoration: BoxDecoration(
+                        border: Border(
+                          top: BorderSide(
+                            color: Colors.grey,
+                            width: 1,
                           ),
                         ),
-                        child: Column(
-                          children: [
-                            Container(
-                              padding: EdgeInsets.only(top: 10, bottom: 10),
-                              child: Row(
-                                children: [
-                                  Icon(
-                                    Icons.access_time,
-                                    color: lawColor,
-                                    size: 14.0,
-                                  ),
-                                  SizedBox(width: 5),
-                                  Text(
-                                    widget.item.createdAt
-                                        .toString()
-                                        .substring(0, 10),
-                                    style: TextStyle(
-                                      color: lawColor,
-                                      fontWeight: FontWeight.w500,
-                                      fontSize: 14,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
                       ),
-                    ],
-                  ),
+                      child: Column(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.only(top: 10, bottom: 10),
+                            child: Row(
+                              children: [
+                                Icon(
+                                  Icons.access_time,
+                                  color: lawColor,
+                                  size: 14.0,
+                                ),
+                                SizedBox(width: 5),
+                                Text(
+                                  widget.item.createdAt
+                                      .toString()
+                                      .substring(0, 10),
+                                  style: TextStyle(
+                                    color: lawColor,
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 14,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
--- lib/ui/views/pages/volunteerWork/volunteer_work_detail.dart
+++ lib/ui/views/pages/volunteerWork/volunteer_work_detail.dart
@@ -4,6 +4,7 @@ import 'package:flutter_html/flutter_html.dart';
 import 'package:flutter_html/style.dart';
 import 'package:youth/core/constants/values.dart';
 import 'package:youth/core/models/volunteer_work.dart';
+import 'package:youth/ui/components/header-back.dart';
 import 'package:youth/ui/styles/_colors.dart';
 import 'package:flutter/rendering.dart';
 
@@ -21,168 +22,142 @@ class _VolunteerWorkDetailState extends State<VolunteerWorkDetail> {
     var size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: bgColor,
-      body: Container(
-        width: double.infinity,
-        decoration: BoxDecoration(
-          color: volunteerColor,
-          image: DecorationImage(
-            image: AssetImage("assets/images/page-heading-legal.jpg"),
-            alignment: Alignment.topRight,
-            //fit: BoxFit.fitWidth,
-          ),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(50.0),
+        child: HeaderBack(
+          title: 'Сайн дурын ажил',
+          reversed: true,
         ),
-        child: Column(
-          children: [
-            Container(
-              height: 180,
-              padding: EdgeInsets.only(top: 60, right: 20),
-              child: Column(
-                children: [
-                  SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      IconButton(
-                        onPressed: () => Navigator.of(context).pop(false),
-                        icon: Icon(
-                          Icons.arrow_back,
-                          color: Colors.white,
-                        ),
-                      ),
-                      Expanded(
-                        child: Text(
-                          widget.item.name != null
-                              ? widget.item.name.toUpperCase()
-                              : 'Сайн дурын ажил'.toUpperCase(),
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.white,
-                            fontWeight: FontWeight.w600,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                ],
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: volunteerWorkColor,
+            ),
+            child: Text(
+              widget.item.name != null
+                  ? widget.item.name.toUpperCase()
+                  : 'Сайн дурын ажил'.toUpperCase(),
+              style: TextStyle(
+                color: Colors.white,
               ),
             ),
-            Expanded(
-              child: SingleChildScrollView(
-                child: Container(
-                  width: double.infinity,
-                  height: size.height,
-                  padding: EdgeInsets.symmetric(horizontal: 15),
-                  decoration: BoxDecoration(
-                    //borderRadius: BorderRadius.circular(10),
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(15),
-                      topRight: Radius.circular(15),
-                    ),
-                    color: bgColor,
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Container(
+                width: double.infinity,
+                height: size.height,
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                decoration: BoxDecoration(
+                  //borderRadius: BorderRadius.circular(10),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15),
                   ),
-                  child: Column(
-                    children: [
-                      SizedBox(height: 10),
-                      widget.item.image != null
-                          ? CachedNetworkImage(
-                              imageUrl: baseUrl + widget.item.image,
-                              imageBuilder: (context, imageProvider) =>
-                                  Container(
-                                decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                    image: imageProvider,
-                                    fit: BoxFit.cover,
-                                  ),
+                  color: bgColor,
+                ),
+                child: Column(
+                  children: [
+                    SizedBox(height: 10),
+                    widget.item.image != null
+                        ? CachedNetworkImage(
+                            imageUrl: baseUrl + widget.item.image,
+                            imageBuilder: (context, imageProvider) => Container(
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: imageProvider,
+                                  fit: BoxFit.cover,
                                 ),
                               ),
-                              placeholder: (context, url) => Center(
-                                child: CircularProgressIndicator(
-                                  strokeWidth: 2,
-                                  backgroundColor: Colors.red,
-                                ),
-                              ),
-                              errorWidget: (context, url, error) =>
-                                  Image.network(
-                                baseUrl + "/assets/youth/images/noImage.jpg",
-                                width: double.infinity,
-                                fit: BoxFit.fitWidth,
-                              ),
-                            )
-                          : Container(
-                              height: 136,
-                              child: Image.network(
-                                baseUrl + "/assets/youth/images/noImage.jpg",
-                                width: double.infinity,
-                                fit: BoxFit.fitWidth,
+                            ),
+                            placeholder: (context, url) => Center(
+                              child: CircularProgressIndicator(
+                                strokeWidth: 2,
+                                backgroundColor: Colors.red,
                               ),
                             ),
-                      SizedBox(height: 10),
-                      Container(
-                        //child: Html(data: widget.news.content),
-                        child: Html(
-                          data: widget.item.description.toString(),
-                          style: {
-                            "p": Style(
-                              color: kTextColor,
-                              fontSize: FontSize(13),
-                              fontWeight: FontWeight.normal,
+                            errorWidget: (context, url, error) => Image.network(
+                              baseUrl + "/assets/youth/images/noImage.jpg",
+                              width: double.infinity,
+                              fit: BoxFit.fitWidth,
                             ),
-                          },
-                        ),
-                      ),
-                      Container(
-                        height: 90,
-                        margin: EdgeInsets.only(
-                          left: 10,
-                          right: 10,
-                          top: 30,
-                          bottom: 10,
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border(
-                            top: BorderSide(
-                              color: Colors.grey,
-                              width: 1,
+                          )
+                        : Container(
+                            height: 136,
+                            child: Image.network(
+                              baseUrl + "/assets/youth/images/noImage.jpg",
+                              width: double.infinity,
+                              fit: BoxFit.fitWidth,
                             ),
                           ),
-                        ),
-                        child: Column(
-                          children: [
-                            Container(
-                              padding: EdgeInsets.only(top: 10, bottom: 10),
-                              child: Row(
-                                children: [
-                                  Icon(
-                                    Icons.access_time,
-                                    color: volunteerColor,
-                                    size: 14.0,
-                                  ),
-                                  SizedBox(width: 5),
-                                  Text(
-                                    widget.item.createdAt
-                                        .toString()
-                                        .substring(0, 10),
-                                    style: TextStyle(
-                                      color: volunteerColor,
-                                      fontWeight: FontWeight.w500,
-                                      fontSize: 14,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
+                    SizedBox(height: 10),
+                    Container(
+                      //child: Html(data: widget.news.content),
+                      child: Html(
+                        data: widget.item.description.toString(),
+                        style: {
+                          "p": Style(
+                            color: kTextColor,
+                            fontSize: FontSize(13),
+                            fontWeight: FontWeight.normal,
+                          ),
+                        },
+                      ),
+                    ),
+                    Container(
+                      height: 90,
+                      margin: EdgeInsets.only(
+                        left: 10,
+                        right: 10,
+                        top: 30,
+                        bottom: 10,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border(
+                          top: BorderSide(
+                            color: Colors.grey,
+                            width: 1,
+                          ),
                         ),
                       ),
-                    ],
-                  ),
+                      child: Column(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.only(top: 10, bottom: 10),
+                            child: Row(
+                              children: [
+                                Icon(
+                                  Icons.access_time,
+                                  color: volunteerColor,
+                                  size: 14.0,
+                                ),
+                                SizedBox(width: 5),
+                                Text(
+                                  widget.item.createdAt
+                                      .toString()
+                                      .substring(0, 10),
+                                  style: TextStyle(
+                                    color: volunteerColor,
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 14,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
